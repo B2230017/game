8345460196a5f360d7625fa767301b7c64fd1c48
B2230017 <164990880+B2230017@users.noreply.github.com>
2024-05-10 22:04:43 +0800

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include "Program.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main() {
+
+    srand(time(nullptr));//創建亂數(answer)
+    int digits[10] = { 0 };
+    int owner = 0;
+    for (int i = 0; i < 4; i++) {
+        int digit;
+        do {
+            digit = rand() % 10;
+        } while (digits[digit]);
+        owner = owner * 10 + digit;
+        digits[digit] = 1;
+    }//創建亂數(answer)
+    Program p(owner);
+    cout << "歡迎來到1A2B" << endl << endl;
+    cout << "請選擇難度(困難按1，簡單按2)" << endl;
+    int n;
+    cin >> n;
+    cout << endl;
+    cout << "請開始遊戲" << endl;
+    if (n == 1) {
+        bool tf = true;
+        for (int i = 0; i < 7; i++) {
+            cout << "請輸入數字:" << endl;
+            int guest;
+            cin >> guest;
+            p.setnumber(guest);
+            if (guest == owner) {
+                cout << "挑戰成功" << endl;
+                tf = false;
+                break;
+            }
+            p.printjudge();
+        }
+        if (tf) {
+            cout << "挑戰失敗" << endl;
+            cout << "答案: " << owner << endl;
+        }
+    }
+    else if (n == 2) {
+        bool tf = true;
+        for (int i = 0; i < 10; i++) {
+            cout << "請輸入數字:" << endl;
+            int guest;
+            cin >> guest;
+            p.setnumber(guest);
+            if (guest == owner) {
+                cout << "恭喜挑戰成功" << endl;
+                tf = false;
+                break;
+            }
+            p.printjudge();
+        }
+        if (tf) {
+            cout << "挑戰失敗" << endl;
+            cout << "答案: " << owner << endl;
+        }
+    }
+
+
+}
